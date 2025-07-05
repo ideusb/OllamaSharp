d6936ef99e9b4591dac8e0f75dfbc58dd26827c3
hrg-work <ideusb@126.com>
2025-07-05 15:19:53 +0800

    add name

--- src/OllamaSharp/OllamaApiClient.cs
+++ src/OllamaSharp/OllamaApiClient.cs
@@ -102,6 +102,11 @@ public class OllamaApiClient : IOllamaApiClient, IChatClient, IEmbeddingGenerato
 	}
 
 	/// <summary>
+	/// Gets or sets the name.
+	/// </summary>
+	public string? Name { get; set; }
+
+	/// <summary>
 	/// Gets or sets an action that is invoked when a function call is made by the chat client.
 	/// </summary>
 	public Action<IChatClient, string, IDictionary<string, object?>?>? OnFunctionCall { get; set; }
